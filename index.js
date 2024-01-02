2a40c06ca13651d18289874d97b9965a59185848
Zainab Faiqua <144930831+zainab3702@users.noreply.github.com>
2024-01-02 11:24:03 +0530

    Created

---
+++ index.js
@@ -0,0 +1,7 @@
+const accordian = document.getElementsByClassName('content-container');
+
+for(i=0; i<accordian.length; i++){
+    accordian[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    })
+}
